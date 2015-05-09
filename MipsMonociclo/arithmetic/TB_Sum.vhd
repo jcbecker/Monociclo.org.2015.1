042a08ab49802cbd2e50ec995ca005e6b003311d
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-05-09 01:06:13 -0300

    Teste do Somador.

--- MipsMonociclo/arithmetic/TB_Sum.vhd
+++ MipsMonociclo/arithmetic/TB_Sum.vhd
@@ -10,36 +10,28 @@ architecture TB_SumAlg of TB_Sum is
 
 	Component Sum is
 		port(
-			clk: in std_logic;
 			a, b: in std_logic_vector( 31 downto 0 );
 			o: out std_logic_vector( 31 downto 0 )
 		);
 	end Component;
 	
-	signal cl: std_logic;
 	signal o1, o2, r: std_logic_vector( 31 downto 0 );
 begin
-	TBSum: Sum port map(cl,o1,o2,r);
+	TBSum: Sum port map(o1,o2,r);
 	process
 	begin
+		wait for 2 ns;
 		o1 <= "00000000000000000000000000000110"; -- 6
 		o2 <= "00000000000000000000000000000111"; -- 7
+		wait for 2 ns;
 
-		cl <= '0';
-		wait for 10 ns;
-		cl <= '1';
-		wait for 10 ns;
-		
 		assert( r = "00000000000000000000000000001101" ) report "Resposta errada linha 33." severity error;
 
+		wait for 2 ns;
 		o1 <= "00000000000000000000000000000001"; -- 1
 		o2 <= "00000000000000000000000000000111"; -- 7
+		wait for 2 ns;
 
-		cl <= '0';
-		wait for 10 ns;
-		cl <= '1';
-		wait for 10 ns;
-		
 		assert( r = "00000000000000000000000000001000" ) report "Resposta errada linha 43." severity error;
 	end process;
 end architecture;
