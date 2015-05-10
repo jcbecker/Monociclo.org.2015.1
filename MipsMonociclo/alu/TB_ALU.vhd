31c4917a6cdede4d5086506101327785e80376ce
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-05-09 22:58:45 -0300

    TB AluControl e ALU

--- MipsMonociclo/alu/TB_ALU.vhd
+++ MipsMonociclo/alu/TB_ALU.vhd
@@ -3,6 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
+--Este TB testa o controle da ula e a ula.
+
 entity TB_ALU is
 end entity;
 
@@ -39,6 +41,58 @@ begin
 	process
 
 	begin
-		
+		wait for 2 ns;
+		na <= "01010101010101010101010101010101";
+		nb <= "10101010101010101010101010101010";
+		AluOpTB <= "10";
+		functTB <= "000100"; --AND
+		wait for 4 ns;
+
+		assert( resulTB = "00000000000000000000000000000000" ) report "Resposta errada teste 0" severity error;
+
+		wait for 2 ns;
+		na <= "01010101010101010101010101010101";
+		nb <= "10101010101010101010101010101010";
+		AluOpTB <= "10";
+		functTB <= "000101"; --OR
+		wait for 4 ns;
+
+		assert( resulTB = "11111111111111111111111111111111" ) report "Resposta errada teste 1." severity error;
+
+		wait for 2 ns;
+		na <= "00000000000000000000000001111111";
+		nb <= "00000000000000000000000000000001";
+		AluOpTB <= "10";
+		functTB <= "000000"; --ADD
+		wait for 4 ns;	
+
+		assert( resulTB = "00000000000000000000000010000000" ) report "Resposta errada teste 2." severity error;
+
+		wait for 2 ns;
+		na <= "00000000000000000000000010000000";
+		nb <= "00000000000000000000000000000001";
+		AluOpTB <= "10";
+		functTB <= "000010"; --subtract
+		wait for 4 ns;	
+
+		assert( resulTB = "00000000000000000000000001111111" ) report "Resposta errada teste 3." severity error;
+
+		wait for 2 ns;
+		na <= "00000000000000000000000010000000";
+		nb <= "00000000000000000000000000000001";
+		AluOpTB <= "10";
+		functTB <= "001010"; --set-on-less-than
+		wait for 4 ns;	
+
+		assert( resulTB = "00000000000000000000000000000000" ) report "Resposta errada teste 4." severity error;
+
+		wait for 2 ns;
+		na <= "00000000000000000000000010000000";
+		nb <= "00000000000000000000000010000000";
+		AluOpTB <= "10";
+		functTB <= "001010"; --repetindo less-than para verificar flag de zero.
+		wait for 4 ns;	
+
+		assert( zeroTB = '1' ) report "Resposta errada teste 5." severity error;
 	end process;
 end architecture;
