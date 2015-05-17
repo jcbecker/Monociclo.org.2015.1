6d866facf9aed225d2501ac722dbbdd65e73794f
jcbecker <joaoc.becker@hotmail.com>
2015-05-17 11:13:25 -0300

    mudança no controle da ula para aceitar slti

--- MipsMonociclo/alu/AluControl.vhd
+++ MipsMonociclo/alu/AluControl.vhd
@@ -18,9 +18,11 @@ begin
 
 	begin
 		if( AluOp = "00" )then
-			operation <= "010";--addi lw sw
+			operation <= "010";--soma para fazer addi lw sw
 		elsif( AluOp = "01" )then
-			operation <= "110";
+			operation <= "110";--sub para fazer beq e bne
+		elsif (AluOp = "10" )then
+			operation <= "111";--slti
 		else
 			if( funct( 3 downto 0 ) = "0000" )then
 				operation <= "010";
