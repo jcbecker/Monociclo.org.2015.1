d27e6e0b214dee9b2c80f724014ae967fbe7c8bb
jcbecker <joaoc.becker@hotmail.com>
2015-05-17 20:26:53 -0300

    adicionado flag JR no AluControl

--- MipsMonociclo/alu/AluControl.vhd
+++ MipsMonociclo/alu/AluControl.vhd
@@ -7,6 +7,7 @@ entity AluControl is
 	port(
 		AluOp: in std_logic_vector( 1 downto 0 );
 		funct: in std_logic_vector( 5 downto 0 );
+		JR: out std_logic;
 		operation: out std_logic_vector( 2 downto 0 )
 	);
 end entity;
@@ -19,21 +20,31 @@ begin
 	begin
 		if( AluOp = "00" )then
 			operation <= "010";--soma para fazer addi lw sw
+			JR <= '0';
 		elsif( AluOp = "01" )then
 			operation <= "110";--sub para fazer beq e bne
+			JR <= '0';
 		elsif (AluOp = "10" )then
 			operation <= "111";--slti
+			JR <= '0';
 		else
 			if( funct( 3 downto 0 ) = "0000" )then
 				operation <= "010";
+				JR <= '0';
 			elsif( funct( 3 downto 0 ) = "0010" )then
 				operation <= "110";
+				JR <= '0';
 			elsif( funct( 3 downto 0 ) = "0100" )then
 				operation <= "000";
+				JR <= '0';
 			elsif( funct( 3 downto 0 ) = "0101" )then
 				operation <= "001";
+				JR <= '0';
 			elsif( funct( 3 downto 0 ) = "1010" )then
 				operation <= "111";
+				JR <= '0';
+			elsif( funct( 3 downto 0 ) = "1000" )then --JR
+				JR <= '1';
 			end if;
 		end if;
 	end process;
