cfd763b40157e8028534c58bf5a89b86a603f63f
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-05-23 10:58:25 -0300

    Novo TB para banco de registradores

--- MipsMonociclo/registers/TB_RegBank.vhd
+++ MipsMonociclo/registers/TB_RegBank.vhd
@@ -10,46 +10,50 @@ architecture TB_RegBankAlg of TB_RegBank is
 
 	component RegisterBank is	
 		port(
-			RegWrite: in std_logic;
+			clkPC, RegWrite: in std_logic;
 			reg1, reg2, writeReg: in std_logic_vector( 4 downto 0 );
 			writeData: in std_logic_vector( 31 downto 0 );
 			data1, data2: out std_logic_vector( 31 downto 0 )
 		);
 	end component;
 
-	signal goWrite: std_logic;
-	signal r1, r2, wr: std_logic_vector( 4 downto 0 );
-	signal data, d1, d2: std_logic_vector( 31 downto 0 );
+	
+	signal clkPC, RegWrite: std_logic;
+	signal reg1, reg2, writeReg: std_logic_vector( 4 downto 0 );
+	signal data1, data2, writeData: std_logic_vector( 31 downto 0 );
 begin
 
-	TBRegBank: RegisterBank port map( goWrite, r1, r2, wr, data, d1, d2 );
+	TBRegBank: RegisterBank port map( clkPC, RegWrite, reg1, reg2, writeReg, writeData, data1, data2 );
 	process
 	begin
-		--preparar teste de escrita.
-		goWrite <= '0';
+		--Escrevendo writeData no registrador 1.
+		clkPC <= '0';
+		RegWrite <= '1';
+		writeReg <= "00001";
+		writeData <= "00000000000000000000000000000011";
+		clkPC <= '1';
 		wait for 2 ns;
-		wr <= "00010";
-		data <= "00000000000000000000000000000010";
-		goWrite <= '1';
-		wait for 4 ns;
 
+		--Lendo o registrador 1 somente para verificar conteudo escrito acima.
+		reg1 <= "00001";
+		RegWrite <= '0';
+		clkPC <= '0';
 		wait for 2 ns;
-		r1 <= "00010";
-		wait for 4 ns;
+		assert( data1 = "00000000000000000000000000000011" ) report "Resposta errada teste 1." severity error;
 
-		assert( d1 = "00000000000000000000000000000010" ) report "Resposta errada teste 1." severity error;
-
-		goWrite <= '0';
+		--Escrevendo writeData no registrador 2.
+		clkPC <= '0';
+		RegWrite <= '1';
+		writeReg <= "00010";
+		writeData <= "00000000000000000000000000000100";
+		clkPC <= '1';
 		wait for 2 ns;
-		wr <= "00001";
-		data <= "00000000000000000000000000000100";
-		goWrite <= '1';
-		wait for 4 ns;
 
+		--Lendo o registrador 2 somente para verificar conteudo escrito acima.
+		reg2 <= "00010";
+		RegWrite <= '0';
+		clkPC <= '0';
 		wait for 2 ns;
-		r2 <= "00001";
-		wait for 4 ns;
-
-		assert( d2 = "00000000000000000000000000000100" ) report "Resposta errada teste 2." severity error;
+		assert( data2 = "00000000000000000000000000000100" ) report "Resposta errada teste 2." severity error;
 	end process;
 end architecture;
