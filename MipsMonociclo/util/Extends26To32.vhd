258366ee8cc809e15fe1b5f1d1a65f35a6d90cba
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-05-23 14:15:43 -0300

    Novo extensor de 26 para 32 bits.

--- MipsMonociclo/util/Extends26To32.vhd
+++ MipsMonociclo/util/Extends26To32.vhd
@@ -2,15 +2,15 @@ library ieee;
 USE ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Extends16To32 is
+entity Extends26To32 is
 	port(
-		e: in std_logic_vector( 25 downto 0 );
+		e26: in std_logic_vector( 25 downto 0 );
 		o: out std_logic_vector( 31 downto 0 )
 	);
 end entity;
 
-architecture Extends16To32Alg of Extends16To32 is
+architecture Extends26To32Alg of Extends26To32 is
 
 begin
-	o <= std_logic_vector(resize( signed(e), 32 ) );
+	o <= std_logic_vector(resize( signed(e26), 32 ) );
 end architecture;
