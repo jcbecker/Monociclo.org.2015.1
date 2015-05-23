7329aae22f2bccca756a8ac8af938c5b8ba53df7
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-05-23 14:16:09 -0300

    Mudança nos nomes da entidade.

--- MipsMonociclo/util/Extends16To32.vhd
+++ MipsMonociclo/util/Extends16To32.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity Extends16To32 is
 	port(
-		e: in std_logic_vector( 15 downto 0 );
+		e16: in std_logic_vector( 15 downto 0 );
 		o: out std_logic_vector( 31 downto 0 )
 	);
 end entity;
@@ -12,5 +12,5 @@ end entity;
 architecture Extends16To32Alg of Extends16To32 is
 
 begin
-	o <= std_logic_vector(resize( signed(e), 32 ) );
+	o <= std_logic_vector(resize( signed(e16), 32 ) );
 end architecture;
